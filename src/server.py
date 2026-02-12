c051c58a0b2d99e2a661bd7a0cd1504d4cc9cfd9
dipanshu-cusp <196902833+dipanshu-cusp@users.noreply.github.com>
2026-02-12 18:50:39 +0530

    feat: dev container

--- src/server.py
+++ src/server.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from database import db
 
 app = FastAPI()
 
+db.do_your_thing()
+
+print("Connected to the database successfully!")
+
 @app.get("/")
-async def root():
+async def root():    
     return {"message": "Hello World"}
